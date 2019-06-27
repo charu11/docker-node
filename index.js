3714f73e3880320fb925e194f1446faa041755d0
charu11 <charunumesh@gmail.com>
2019-06-27 10:09:32 +0530

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+
 
 const app = express();
+
+var Item = require('./models/Item');
 
 app.set('view engine', 'ejs');
 
@@ -11,28 +15,61 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Connect to MongoDB
 mongoose
   .connect(
-    'mongodb://mongo:27017/docker-node-mongo',
+    'mongodb://mongo:27020/docker-node-mongo',
     { useNewUrlParser: true }
   )
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+  .then(function(){
+    console.log('MongoDB Connected')
+  })
+  .catch( function(err){
+    console.log(err)
+  });
 
-const Item = require('./models/Item');
 
-app.get('/', (req, res) => {
+
+app.get('/', function(req, res){
   Item.find()
-    .then(items => res.render('index', { items }))
-    .catch(err => res.status(404).json({ msg: 'No items found' }));
+  .then(function(items){
+    res.render('index', { items })
+  })
+  .catch(function(err){
+    res.status(404).json({ msg: 'No items found' });
+  });
+
+ 
 });
 
-app.post('/item/add', (req, res) => {
-  const newItem = new Item({
+app.post('item/add', function(req, res){
+  var newItem = new Item();
+  newItem.name = req.body.name
+
+  newItem.save(function(err){
+    if(err){
+      console.log(err);
+      console.log('error occured')
+    }else{
+      console.log('success the item has been to the database')
+    }
+  })
+})
+/*
+app.post('/item/add', function(req, res){
+  var newItem = new Item({
     name: req.body.name
   });
 
-  newItem.save().then(item => res.redirect('/'));
+  newItem.save().then(function(item){
+    res.redirect('/');
+   
 });
+*/
+var port = 3000;
 
-const port = 3000;
 
-app.listen(port, () => console.log('Server running...'));
+app.listen(port, function(err){
+  if(err){
+    console.log(err, "error occured cant connect to port : 3000");
+  }else{
+    console.log("server is up and running");
+  }
+});
